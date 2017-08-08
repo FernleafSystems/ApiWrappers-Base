27d366da3d6da357edbbcc073796b725170024b7
Paul Goodchild <paul@icontrolwp.com>
2017-08-08 10:10:45 +0100

    allow public access to response body

--- src/BaseApi.php
+++ src/BaseApi.php
@@ -62,12 +62,19 @@ abstract class BaseApi {
 	 * Assumes response body is json-encode.  Override for anything else
 	 * @return array
 	 */
-	protected function getDecodedResponseBody() {
+	public function getDecodedResponseBody() {
 		$sResponse = array();
 		if ( !$this->hasError() ) {
-			$sResponse = json_decode( $this->getLastApiResponse()->getBody()->getContents(), $this->isDecodeAsArray() );
+			$sResponse = json_decode( $this->getResponseBodyContentRaw(), $this->isDecodeAsArray() );
 		}
 		return $sResponse;
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getResponseBodyContentRaw() {
+		return $this->getLastApiResponse()->getBody()->getContents();
 	}
 
 	/**
