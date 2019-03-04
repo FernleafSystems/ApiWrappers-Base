c1c2a862da595d2b4a21d82230982b285b3bcd44
Paul Goodchild <paul@icontrolwp.com>
2019-03-04 12:07:48 +0000

    improve the connection class

--- src/Connection.php
+++ src/Connection.php
@@ -5,26 +5,31 @@ namespace FernleafSystems\ApiWrappers\Base;
 /**
  * Class Connection
  * @package FernleafSystems\ApiWrappers\Base
+ * @property string api_key
  */
 class Connection {
 
+	use \FernleafSystems\Utilities\Data\Adapter\StdClassAdapter;
+
 	/**
+	 * @deprecated
 	 * @var string
 	 */
 	protected $sApiKey;
 
 	/**
+	 * @deprecated
 	 * @return string
 	 */
 	public function getApiKey() {
-		return $this->sApiKey;
+		return isset( $this->api_key ) ? $this->api_key : $this->sApiKey;
 	}
 
 	/**
 	 * @return bool
 	 */
 	public function hasApiKey() {
-		return !empty( $this->sApiKey );
+		return !empty( $this->api_key ) || !empty( $this->sApiKey );
 	}
 
 	/**
@@ -32,6 +37,7 @@ class Connection {
 	 * @return $this
 	 */
 	public function setApiKey( $sApiKey ) {
+		$this->api_key = $sApiKey;
 		$this->sApiKey = $sApiKey;
 		return $this;
 	}
